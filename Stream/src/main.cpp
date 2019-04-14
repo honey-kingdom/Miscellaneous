05d2fbc8335c4f1c5a259bad5a1e7c80494b742a
Honey Kingdom <honey.kingdom00@gmail.com>
2019-04-14 17:36:14 +0900

    Open file with a speical option using its constructor - std::ios::app for appending new contents to an existing file - is_opened() to check if the file has been successfully opened.

--- Stream/src/main.cpp
+++ Stream/src/main.cpp
@@ -3,11 +3,17 @@
 
 int main(void)
 {
-	std::ofstream myfile;
+	std::ofstream myfile("out/example.txt", std::ios::out | std::ios::app);
+	// Given option overrides the default value of fstream but is combined with the default value of ofstream/ifstream.
 	
-	myfile.open("out/example.txt");
-	myfile << "Writing this to a file.\n";
-	myfile.close();
-	
+	if (myfile.is_open())
+	{
+		myfile << "This is a line.\n";
+		myfile << "This is another line.\n";
+		myfile.close();		
+	}
+	else std::cout << "Unable to open file";
+
+	std::cin.get();	
 	return 0;
 }
